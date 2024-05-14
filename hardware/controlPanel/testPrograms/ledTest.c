d470dcefc6661ab66a0445a43711f2e73282957c
Martin Feichtinger <martin.feichtinger22@gmail.com>
2024-05-14 17:14:32 +0200

    added a test programm for the leds on the controlPanel

---
+++ hardware/controlPanel/testPrograms/ledTest.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <pigpiod_if2.h>
+#include <unistd.h>		
+
+
+#define GPS_LED		17
+#define BOOT_LED	27
+#define NET_LED		22
+
+void msleep(int milliseconds);
+void blink(int gpio, float period, int repetition);
+
+int main(){
+	// init gpio client
+	const int pi = pigpio_start(NULL, NULL);
+	if(pi < 0){
+		printf("Raspberry Pi not found");
+		return;
+	}
+
+	// init gpios
+	set_mode(pi, GPS_LED, PI_OUTPUT);
+	set_mode(pi, BOOT_LED, PI_OUTPUT);
+	set_mode(pi, NET_LED, PI_OUTPUT);
+
+
+	// let the boot led blink for 2 seconds
+	// (The boot LED is controlled by an lm555. During booting, the boot LED flashes and after booting is complete,
+	// the BOOT_LED pin must be set to low so that the LED remains lit)
+	gpio_write(pi, BOOT_LED, 1);
+	msleep(2000);
+	gpio_write(pi, BOOT_LED, 0);
+
+	// let the GPS_LED blink for 2 seconds
+	blink(GPS_LED, 1000, 2);
+
+	// let the NET_LED blink for 2 seconds
+	blink(NET_LED, 1000, 2);
+
+	printf("finished ledTest\n");
+	return 0;
+}
+
+void blink(int gpio, float period, int repetition){
+	for(int i=0; i<repetition; i++){
+		gpio_write(pi, gpio, 1);
+		msleep(period/2);
+		gpio_write(pi, gpio, 0);
+		msleep(period/2);		
+	}
+}
+
+
+void msleep(int milliseconds){
+	usleep(milliseconds*1000);
+}
