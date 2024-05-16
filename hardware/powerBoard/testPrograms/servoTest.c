756cda19b94d0770b0a02dc171203e9a6f00363e
Martin Feichtinger <martin.feichtinger22@gmail.com>
2024-05-16 20:25:19 +0200

    improved the servo test programm

--- hardware/powerBoard/testPrograms/servoTest.c
+++ hardware/powerBoard/testPrograms/servoTest.c
@@ -5,6 +5,9 @@
 #define PHASE	18
 #define NFAULT	23
 
+
+void nfault_cb(int pi, unsigned user_gpio, unsigned edge, unint32_t tick);		// callback function (is called when the nFault state changes)
+
 int main(){
     // init gpio client
     const int pi = pigpio_start(NULL, NULL);
@@ -18,12 +21,44 @@ int main(){
     set_mode(pi, PHASE, PI_OUTPUT);
     set_mode(pi, NFAULT, PI_INPUT);
 
+	// cofigurate the pwm
+	int pwm_frequency = 8000;
+	int real_pwm_frequency = set_PWM_frequency(pi, ENABLE, pwm_frequency)
+	printf("pwm_frequency: set=%d; real=%d\n", pwm_frequency, real_pwm_frequency);
+
+	int pwm_range = 100;
+	int real_pwm_range = set_PWM_range(pi, ENABLE, 100);	// => 1 = 1%
+	printf("pwm_range: set=%d; real=%d\n", pwm_range, real_pwm_range);
+
+	// init NFAULT callback function
+	const int nfault_cb_id = callback(pi, NFAULT, EITHER_EDGE, nfault_cb);
+    if(nfault_cb_id < 0){
+        printf("init callback failed");
+        return;
+    }
+
     // turn on the motor
     gpio_write(pi, PHASE, 1);
-    gpio_write(pi, ENABLE, 1);
+    set_PWM_dutycycle(pi, ENABLE, 70);
 
-    while(gpio_read(pi, NFAULT))	// wait until the torque gets to high
-    printf("nFault turned low");
-
+    // wait until the torque gets to high 
+	// stopping the motor is handled with a special callback function
+    while(1){
+		time_sleep(1);
+	}
+	
 	return 0;
+}
+
+void nfault_cb(int pi, unsigned user_gpio, unsigned edge, unint32_t tick){
+	if(edge == FALLING_EDGE){
+		printf("nFault turned LOW\n");
+		//set_PWM_dutycycle(pi, ENABLE, 0);		// stop the motor
+	}
+	else if(edge == RISING_EDGE){
+		printf("nFault turned HIGH\n");
+	}
+	else{
+		printf("some wierd timeout occured\n");
+	}
 }
