6ede20a27989a93d4c9baf2a9f7aee455be655d3
Martin Feichtinger <martin.feichtinger22@gmail.com>
2024-05-18 21:36:51 +0100

    fixed the button test programm

--- hardware/controlPanel/testPrograms/buttonTest.c
+++ hardware/controlPanel/testPrograms/buttonTest.c
@@ -6,40 +6,40 @@
 #define NET_LED	22
 
 
-void button_cb(int pi, unsigned user_gpio, unsigned edge, unint32_t tick);		// callback function (is called when the button state changes)
+void button_cb(int pi, unsigned user_gpio, unsigned edge, uint32_t tick);		// callback function (is called when the button state changes)
 
 int main(){
-    // init gpio client
-    const int pi = pigpio_start(NULL, NULL);
-    if(pi < 0){
-        printf("Raspberry Pi not found");
-        return;
-    }
+	// init gpio client
+	const int pi = pigpio_start(NULL, NULL);
+	if(pi < 0){
+		printf("Raspberry Pi not found");
+		return -1;
+	}
 
-    // init gpios
+	// init gpios
 	set_mode(pi, NET_LED, PI_OUTPUT);
 	set_mode(pi, BUTTON, PI_INPUT);
 	set_glitch_filter(pi, BUTTON, 20000);	// ingor all input changes for 20ms after the input state changes
 	const int cb_id = callback(pi, BUTTON, EITHER_EDGE, button_cb);
-    if(cb_id < 0){
-        printf("init callback failed");
-        return;
-    }
+	if(cb_id < 0){
+		printf("init callback failed");
+		return -1;
+	}
 
-    while(1){
+	while(1){
 		time_sleep(1);
-    }
+	}
 
 	return 0;
 }
 
-void button_cb(int pi, unsigned user_gpio, unsigned edge, unint32_t tick){
+void button_cb(int pi, unsigned user_gpio, unsigned edge, uint32_t tick){
 	if(edge == RISING_EDGE){
 		gpio_write(pi, NET_LED, 1);
 		printf("Button pressed\n");
 	}
 	else if(edge == FALLING_EDGE){
 		gpio_write(pi, NET_LED, 0);
-		printf("Button released\n");		
+		printf("Button released\n");
 	}
 }
